30b846693472fea1640a49abaf87bff1f84f46d4
Danila Manakov <manakov.danila@mail.ru>
2022-04-18 02:34:04 +0300

    add: sandbox; now it works!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,126 @@
 #include "phys.h"
 #include "renderer.h"
+#include <cassert>
+#include <cmath>
 #include <memory>
 
-int main(int argc, char *args[]) {
-  BodyCreator bc;
-  bc.addPoint(Vec2(.1f, .1f));
-  bc.addPoint(Vec2(.9f, .1f));
-  bc.addPoint(Vec2(.9f, .6f));
-  bc.addPoint(Vec2(.5f, .8f));
-  bc.addPoint(Vec2(.1f, .6f));
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 640
 
-  Body body = bc.createBody();
+#define _2PI 2 * 3.1415926
 
-  std::shared_ptr<Window> window(new SDLWindow(640, 640));
+const Vec2 pix2coords(int x, int y) {
+  return Vec2(float(x) / WINDOW_WIDTH, 1. - float(y) / WINDOW_HEIGHT);
+}
 
+void drawAndExit(Window *window, const Body &body) {
   window->Clear();
   window->drawBody(&body);
   window->Present();
 
   SDL_Delay(2000);
+  exit(1);
+}
+
+void NGon(BodyCreator &bc, int N, const Vec2 &center, float radius) {
+  bc.clear();
+  for (int i = 0; i < N; ++i) {
+    const Vec2 dp =
+        Vec2(radius * cos(i * _2PI / N), radius * sin(i * _2PI / N));
+    const Vec2 p = center + dp;
+
+    bc.addPoint(p);
+  }
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (i == j) {
+        continue;
+      }
+
+      bc.addEdge(i, j);
+    }
+  }
+}
+
+int main(int argc, char *args[]) {
+  Sandbox sandbox;
+  sandbox.setAcceleration(Vec2(.0f, .1f));
+
+  BodyCreator bcreator;
+
+  std::shared_ptr<Window> window(new SDLWindow(640, 640));
+
+  // some body
+  bcreator.addPoint(Vec2(.4f, .4f));
+  bcreator.addPoint(Vec2(.6f, .4f));
+  bcreator.addPoint(Vec2(.6f, .6f));
+  bcreator.addPoint(Vec2(.4f, .6f));
+  auto bd = bcreator.createBody();
+  auto p = Point(Vec2(.5, .5));
+  assert(isPointInsideBody(&p, &bd));
+
+  // bottom border
+  bcreator.clear();
+  bcreator.addPoint(pix2coords(0, 0));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, 0));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, 120));
+  bcreator.addPoint(pix2coords(0, 120));
+  // drawAndExit(&*window, bcreator.createBody());
+  sandbox.addStaticBody(bcreator.createBody());
+
+  // top border
+  bcreator.clear();
+  bcreator.addPoint(pix2coords(0, WINDOW_HEIGHT));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, WINDOW_HEIGHT));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, WINDOW_HEIGHT - 20));
+  bcreator.addPoint(pix2coords(0, WINDOW_HEIGHT - 20));
+  // drawAndExit(&*window, bcreator.createBody());
+  sandbox.addStaticBody(bcreator.createBody());
+
+  // left border
+  bcreator.clear();
+  bcreator.addPoint(pix2coords(0, 0));
+  bcreator.addPoint(pix2coords(20, 0));
+  bcreator.addPoint(pix2coords(20, WINDOW_HEIGHT));
+  bcreator.addPoint(pix2coords(0, WINDOW_HEIGHT));
+  // drawAndExit(&*window, bcreator.createBody());
+  sandbox.addStaticBody(bcreator.createBody());
+
+  // right border
+  bcreator.clear();
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, 0));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH - 20, 0));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH - 20, WINDOW_HEIGHT));
+  bcreator.addPoint(pix2coords(WINDOW_WIDTH, WINDOW_HEIGHT));
+  // drawAndExit(&*window, bcreator.createBody());
+  sandbox.addStaticBody(bcreator.createBody());
+
+  // 'ball'
+  bcreator.clear();
+  NGon(bcreator, 4, pix2coords(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), .1f);
+  // drawAndExit(&*window, bcreator.createBody());
+  sandbox.addDynamicBody(bcreator.createBody());
+
+  // std::shared_ptr<Window> window(new SDLWindow(640, 640));
+
+  while (true) {
+    sandbox.update(.02);
+
+    window->Clear();
+
+    for (const Body &body : sandbox.getStaticBodys()) {
+      window->drawBody(&body);
+    }
+
+    for (const Body &body : sandbox.getDynamicBodys()) {
+      window->drawBody(&body);
+    }
+
+    window->Present();
+
+    SDL_Delay(20);
+  }
+
   return 0;
 }
--- src/phys.h
+++ src/phys.h
@@ -36,28 +36,27 @@ struct Point {
 struct Edge {
   struct i_promise_do_not_use_as_edge {};
 
-  const Point *const first;
-  const Point *const second;
+  Point *const first;
+  Point *const second;
 
   float length;
   float rigidity;
 
-  Edge(const Point *const first, const Point *const second,
+  Edge(Point *const first, Point *const second,
        i_promise_do_not_use_as_edge /**/)
       : first(first), second(second), length(-1.f), rigidity(-1.f) {}
 
-  Edge(const Point *const first, const Point *const second)
+  Edge(Point *const first, Point *const second)
       : first(first), second(second),
         length((first->current_position - second->current_position).abs()),
         rigidity(1.) {}
 
-  Edge(const Point *const first, const Point *const second, float rigidity)
+  Edge(Point *const first, Point *const second, float rigidity)
       : first(first), second(second),
         length((first->current_position - second->current_position).abs()),
         rigidity(rigidity) {}
 
-  Edge(const Point *const first, const Point *const second, float length,
-       float rigidity)
+  Edge(Point *const first, Point *const second, float length, float rigidity)
       : first(first), second(second), length(length), rigidity(rigidity) {}
 };
 
@@ -70,9 +69,14 @@ struct Body {
 
   BodyType type;
 
+  Body() = default;
+  Body(const Body &) = default;
+  Body(Body &&) = default;
+
   ~Body() {
-    schreder<Point>(points);
-    schreder<Edge>(edges);
+    // TODO: don't forget to remove it нахрен
+    // schreder<Point>(points);
+    // schreder<Edge>(edges);
   }
 };
 
@@ -87,6 +91,7 @@ bool isPointsOnSameSide(const Point *const p0, const Point *const p1,
 }
 
 // !!! STATIC BODY ONLY !!!
+/*
 bool isPointInsideBody(const Point *const p0, const Body *const body) {
   Vec2 center = Vec2(0.f, 0.f);
   for (int i = 0; i < body->points_count; ++i) {
@@ -105,13 +110,43 @@ bool isPointInsideBody(const Point *const p0, const Body *const body) {
   }
   return true;
 }
+*/
+
+bool isPointsOnSameSideOfLine(Vec2 p0, Vec2 p1, Vec2 line_beg, Vec2 line_end) {
+  Vec2 n0 = normal(p0, line_beg, line_end);
+  Vec2 n1 = normal(p1, line_beg, line_end);
+
+  return n0 * n1 > 0;
+}
+
+bool isPointInsideBody(const Point *const p0, const Body *const body) {
+  Vec2 p = p0->current_position;
+  const Body *const b = body;
+  Vec2 b_center;
+  for (int i = 0; i < b->points_count; i++)
+    b_center = b_center + b->points[i].current_position;
+  b_center = b_center / b->points_count;
+
+  bool check = true;
+
+  for (int i = 0; i < b->points_count - 1; i++)
+    check =
+        check &&
+        (isPointsOnSameSideOfLine(p, b_center, b->points[i].current_position,
+                                  b->points[i + 1].current_position));
+  check = check && (isPointsOnSameSideOfLine(
+                       p, b_center, b->points[0].current_position,
+                       b->points[b->points_count - 1].current_position));
+
+  return check;
+}
 
 class BodyCreator {
 public:
-  void addPoint(Vec2 point) { points.emplace_back(Point(point)); }
+  void addPoint(Vec2 point) { points.push_back(Point(point)); }
 
   void addEdge(int point0_id, int point1_id) {
-    edges.emplace_back(std::make_pair(point0_id, point1_id));
+    edges.push_back(std::make_pair(point0_id, point1_id));
   }
 
   void clear() {
@@ -141,6 +176,125 @@ private:
   std::vector<std::pair<int, int>> edges;
 
   float rigidity = 1.f;
+};
+
+class Sandbox {
+public:
+  Sandbox() {}
+
+  ~Sandbox() {}
+
+  void setAcceleration(const Vec2 &acc) { acceleration = acc; }
+
+  void addDynamicBody(Body &&body) {
+    dynamic_bodys.push_back(body);
+
+    Body &last = *dynamic_bodys.rbegin();
+    for (int i = 0; i < last.points_count; ++i) {
+      last.points[i].acceleration = acceleration;
+    }
+  }
+
+  void addStaticBody(Body &&body) { static_bodys.push_back(body); }
+
+  void update(float dtime) {
+    for (Body &body : dynamic_bodys) {
+      updateBody(body, dtime);
+    }
+
+    for (Body &body : dynamic_bodys) {
+      updateEdges(body, dtime);
+    }
+
+    for (Body &body : dynamic_bodys) {
+      updateCollisions(body, dtime);
+    }
+  }
+
+  const std::vector<Body> getStaticBodys() const { return static_bodys; }
+
+  const std::vector<Body> getDynamicBodys() const { return dynamic_bodys; }
+
+private:
+  const Vec2 getNormalToBody(const Point &point, const Body &body) {
+    Vec2 norm = normal(point.current_position, body.points[0].current_position,
+                       body.points[1].current_position);
+
+    for (int i = 1; i < body.points_count; ++i) {
+      int j = (i + 1) % body.points_count;
+
+      Vec2 candidate =
+          normal(point.current_position, body.points[i].current_position,
+                 body.points[j].current_position);
+
+      if (candidate.sqr_abs() < norm.sqr_abs()) {
+        norm = candidate;
+      }
+    }
+
+    return norm;
+  }
+
+  void updateBody(Body &body, float dtime) {
+    for (int i = 0; i < body.points_count; ++i) {
+      Point &p = body.points[i];
+
+      // TODO: fix time error
+      const Vec2 new_pos =
+          p.current_position +
+          (p.current_position - p.previous_position) + // should be 'V * dtime'
+          p.acceleration * dtime * dtime;
+      p.previous_position = p.current_position;
+      p.current_position = new_pos;
+    }
+  }
+
+  void updateEdges(Body &body, float dtime) {
+    // TODO: update time error
+    for (int i = 0; i < body.edges_count; ++i) {
+      Edge &edge = body.edges[i];
+
+      // TODO: fix prev_position error
+      const Vec2 edge_center =
+          .5 * (edge.first->current_position + edge.second->current_position);
+
+      const Vec2 edge_cener_to_first =
+          edge.first->current_position - edge_center;
+      const Vec2 edge_cener_to_second =
+          edge.second->current_position - edge_center;
+
+      edge.first->current_position =
+          edge_center +
+          edge_cener_to_first.normalize() * edge.length * .5 * edge.rigidity;
+      edge.second->current_position =
+          edge_center +
+          edge_cener_to_second.normalize() * edge.length * .5 * edge.rigidity;
+    }
+  }
+
+  void updateCollisions(Body &body, float dtime) {
+    for (int i = 0; i < body.points_count; ++i) {
+      for (const Body &static_body : static_bodys) {
+        if (not isPointInsideBody(&body.points[i], &static_body)) {
+          continue;
+        }
+
+        const Vec2 curr_shift =
+            getNormalToBody(body.points[i].current_position, static_body);
+        const Vec2 prev_shift =
+            getNormalToBody(body.points[i].previous_position, static_body);
+
+        // TODO: fix previous positoin error
+        body.points[i].current_position += 2.f * curr_shift;
+        body.points[i].previous_position += 2.f * prev_shift;
+      }
+    }
+  }
+
+  Vec2 acceleration;
+
+  std::vector<Body> static_bodys;
+  std::vector<Body> dynamic_bodys;
 };
 
 #endif // PHYS_H
